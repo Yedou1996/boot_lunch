24905347497c28f2dedc6396ecdddd88de7ee991
LXL <328161507@qq.com>
2019-12-16 11:03:01 +0800

    Hello

---
+++ src/main/java/com/lxl/boot_lunch/BootLunchApplication.java
@@ -0,0 +1,13 @@
+package com.lxl.boot_lunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLunchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lxl/boot_lunch/BootLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxl.boot_lunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
